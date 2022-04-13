37a988f9c79b72629794f23113104d44c743863c
Akash Cherukuri <190050009@iitb.ac.in>
2022-04-13 16:19:59 +0530

    Add run.sh to make running stuff easier

---
+++ dblayer/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ..
+
+cd pflayer && make clean && make && cd ..
+cd amlayer && make clean && make && cd ..
+cd dblayer && make clean && make 
+
+./interface
+
+make clean
+cd .. && cd amlayer && make clean
+cd .. && cd pflayer && make clean
+cd ../dblayer
