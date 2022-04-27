a4b9009f7af1664e4d787b0d57931ecbc0b9fb1b
Amit Rajaraman <amit.rajaraman@gmail.com>
2022-04-27 20:21:01 +0530

    lock manager fix

--- dblayer/lockManager.c
+++ dblayer/lockManager.c
@@ -1,14 +1,14 @@
 #include "lockManager.h"
 
-lockManager::getLocks(vector<pair<std::string,int>> requestedLocks) {
-	accessLock.lock(); // is this needed?
-	vector<counting_semaphore> semVec;
+std::vector<sem_t*> lockManager::getLocks(std::vector<std::pair<std::string,int>> requestedLocks) {
+	sem_wait(accessLock);
+	std::vector<sem_t*> semVec;
 	for (auto tbl : requestedLocks) {		
 		if(tbl.second == 0)
 			semVec.push_back(lockMap[tbl.first].xLock);
 		else
 			semVec.push_back(lockMap[tbl.first].sLock);
 	}
-	accessLock.unlock(); // is this needed?
+	sem_post(accessLock);
 	return semVec;
 }
--- dblayer/lockManager.h
+++ dblayer/lockManager.h
@@ -1,29 +1,49 @@
 #ifndef _LOCKMANAGER
 #define _LOCKMANAGER
 
-#include <vector>
-#include <map>
 #include <string>
-#include <mutex>
-#include <semaphore> // need to install some "linux-headers" stuff, not there on WSL?
+#include <map>
+#include <vector>
+#include <semaphore.h>
+#include <pthread.h>
 
-typedef struct {
-	std::counting_semaphore xLock<1>;
-	std::counting_semaphore sLock{0};
+sem_t* make_semaphore(int value)
+{
+    sem_t* sem = new sem_t;
+    int n = sem_init(sem, 0, value);
+	// if (n != 0) perror_exit("sem_init failed");
+    return sem;
+}
+
+
+class lockObject {
+public:
+	sem_t *xLock;
+	sem_t *sLock;
 	int lockCount;
-} lockObject;
 
-typedef struct {
-	map<std::string,lockObject> lockMap;
-	std::mutex accessLock;
-} lockManager;
+	lockObject() {
+		this->xLock = make_semaphore(1);
+		this->sLock = make_semaphore(-1);
+	}
+};
 
+class lockManager {
+public:
+	std::map<std::string,lockObject> lockMap;
+	sem_t *accessLock;
 
-/*
-Each element of the vector has a table name and an int.
-If the int is 0, an exclusive lock is being requested and if it is 1, a shared lock is being requested.
-The entries are in lexicographic order (in the string) to prevent deadlocks
-*/
-vector<std::counting_semaphore> lockManager::getLocks(vector<pair<std::string,int>>);
+	lockManager() {
+		this->accessLock = make_semaphore(1);
+	}
+
+	/*
+	Each element of the vector has a table name and an int.
+	If the int is 0, an exclusive lock is being requested and if it is 1, a shared lock is being requested.
+	The entries are in lexicographic order (in the string) to prevent deadlocks
+	*/
+	std::vector<sem_t*> getLocks(std::vector<std::pair<std::string,int>>);
+
+};
 
 #endif
