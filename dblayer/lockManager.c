5d68ef1a371438d0181c83154954b14cd79a08ff
Amit Rajaraman <amit.rajaraman@gmail.com>
2022-04-28 17:16:42 +0530

    lockManager

--- dblayer/lockManager.c
+++ dblayer/lockManager.c
@@ -1,14 +1,61 @@
 #include "lockManager.h"
 
-std::vector<sem_t*> lockManager::getLocks(std::vector<std::pair<std::string,int>> requestedLocks) {
-	sem_wait(accessLock);
+sem_t* make_semaphore(int value)
+{
+    sem_t* sem = new sem_t;
+    int n = sem_init(sem, 0, value);
+	// if (n != 0) perror_exit("sem_init failed");
+    return sem;
+}
+
+lockObject::lockObject() {
+	this->xLock = make_semaphore(1);
+	this->sLock = make_semaphore(-1);
+	this->xAcquired = 0;
+}
+
+lockManager::lockManager() {
+	this->lmLock = PTHREAD_MUTEX_INITIALIZER;
+	this->lmCond = PTHREAD_COND_INITIALIZER;
+}
+
+int lockManager::getLocks(int clientId, std::vector<std::pair<std::string,int>> requestedLocks) {
+	pthread_mutex_lock(&lmLock);
 	std::vector<sem_t*> semVec;
-	for (auto tbl : requestedLocks) {		
-		if(tbl.second == 0)
-			semVec.push_back(lockMap[tbl.first].xLock);
+	for (auto tbl : requestedLocks) {
+		if(tbl.second == 0) {
+			while(lockMap[tbl.first].xAcquired != 0)
+				pthread_cond_wait(&lmCond, &lmLock);
+			sem_wait(lockMap[tbl.first].sLock);
+		}
+		else if(tbl.second == 1) {
+			while(lockMap[tbl.first].lockCount > 0)
+				pthread_cond_wait(&lmCond, &lmLock);
+			sem_wait(lockMap[tbl.first].xLock);
+			lockMap[tbl.first].xAcquired = 1;
+		}
 		else
-			semVec.push_back(lockMap[tbl.first].sLock);
+			return 1;
 	}
-	sem_post(accessLock);
-	return semVec;
+	pthread_mutex_unlock(&lmLock);
+	return 0;
+}
+
+int lockManager::releaseLocks(int clientId, std::vector<std::pair<std::string,int>> requestedLocks) {
+	pthread_mutex_lock(&lmLock);
+	for (auto tbl : requestedLocks) {
+		if(tbl.second == 0) {
+			sem_post(lockMap[tbl.first].sLock);
+			--lockMap[tbl.first].lockCount;
+		}
+		else if(tbl.second == 1) {
+			sem_post(lockMap[tbl.first].xLock);
+			lockMap[tbl.first].xAcquired = 0;
+		}
+		else
+			return 1;
+	}
+	pthread_cond_signal(&lmCond);
+	pthread_mutex_unlock(&lmLock);
+	return 0;
 }
--- dblayer/lockManager.h
+++ dblayer/lockManager.h
@@ -7,42 +7,33 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-sem_t* make_semaphore(int value)
-{
-    sem_t* sem = new sem_t;
-    int n = sem_init(sem, 0, value);
-	// if (n != 0) perror_exit("sem_init failed");
-    return sem;
-}
-
+sem_t* make_semaphore(int);
 
 class lockObject {
 public:
 	sem_t *xLock;
 	sem_t *sLock;
+	int xAcquired;
 	int lockCount;
 
-	lockObject() {
-		this->xLock = make_semaphore(1);
-		this->sLock = make_semaphore(-1);
-	}
+	lockObject();
 };
 
 class lockManager {
 public:
+	pthread_cond_t lmCond;
+	pthread_mutex_t lmLock;
 	std::map<std::string,lockObject> lockMap;
-	sem_t *accessLock;
-
-	lockManager() {
-		this->accessLock = make_semaphore(1);
-	}
+	
+	lockManager();
 
 	/*
 	Each element of the vector has a table name and an int.
 	If the int is 0, an exclusive lock is being requested and if it is 1, a shared lock is being requested.
 	The entries are in lexicographic order (in the string) to prevent deadlocks
 	*/
-	std::vector<sem_t*> getLocks(std::vector<std::pair<std::string,int>>);
+	int getLocks(int, std::vector<std::pair<std::string,int>>);
+	int releaseLocks(int, std::vector<std::pair<std::string,int>>);
 
 };
 
